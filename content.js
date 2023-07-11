42c4a0b7f85c310dd82ff21ca936d376f3c8016f
Steven Steinwand <steven_ts@hotmail.com>
2023-07-11 13:26:36 -0600

    Fix typos etc

--- content.js
+++ content.js
@@ -1,14 +1,29 @@
+function searchForText(element, searchText) {
+  if (element.textContent.includes(searchText)) {
+    return element;
+  }
+
+  for (let i = 0; i < element.children.length; i++) {
+    const foundElement = searchForText(element.children[i], searchText);
+    if (foundElement) {
+      return foundElement;
+    }
+  }
+
+  return null;
+}
 
 chrome.storage.sync.get(['linesToHide'], function(result) {
     const linesToHide = result.linesToHide;
     if (linesToHide) {
       const lines = linesToHide.split(',');
       lines.forEach(function(line) {
-        const elements = document.querySelectorAll(`:contains("${line.trim()}")`);
-        elements.forEach(function(element) {
-            const parentButton = element.closest('button');
-            if (parentButton) {
-                parentButton.style.dispaly = 'none';
+        const list = document.querySelector('[data-qa="wstrade-position-list"]');
+        const buttons = list.querySelectorAll('button');
+        buttons.forEach(function(button) {
+            const foundElement = searchForText(button, line);
+            if (foundElement) {
+              foundElement.style.display = 'none';
             }
         });
       });
--- options.js
+++ options.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('hideTickersForm');
+
+    chrome.storage.sync.get(['linesToHide'], function(result) {
+      const linesToHide = result.linesToHide;
+      const linesToHideInput = document.getElementById('linesToHide');
+      linesToHideInput.value = linesToHide;
+    })
+
     form.addEventListener('submit', function(event) {
       event.preventDefault();
       const linesToHide = document.getElementById('linesToHide').value;
